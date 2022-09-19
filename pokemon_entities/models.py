19850518820220b29dd5399c812fad036f34253d
Atmoslayer <ddsomdim@gmail.com>
2022-09-19 21:43:42 +0300

    Added related_name

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -13,20 +13,21 @@ class Pokemon(models.Model):
         null=True,
         blank=True,
         verbose_name='Из кого эволюционирует',
-        related_name='pokemons')
+        related_name='previous')
     next_evolution = models.ForeignKey(
         'self',
         on_delete=models.CASCADE,
         null=True,
         blank=True,
-        verbose_name='В кого эволюционирует')
+        verbose_name='В кого эволюционирует',
+        related_name='next')
 
     def __str__(self):
         return self.title
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, null=True)
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, null=True, related_name='pokemons')
     appeared_at = models.DateTimeField(null=True, verbose_name='Время появления')
     disappeared_at = models.DateTimeField(null=True, verbose_name='Время исчезновения')
     lat = models.FloatField(max_length=10, verbose_name='Широта')
@@ -36,3 +37,4 @@ class PokemonEntity(models.Model):
     strength = models.IntegerField(null=True, blank=True, verbose_name='Сила')
     defence = models.IntegerField(null=True, blank=True, verbose_name='Защита')
     stamina = models.IntegerField(null=True, blank=True, verbose_name='Выносливость')
+
