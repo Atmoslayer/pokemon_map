af6698f2f6f94cc121145202ed33cad39026a5f7
Atmoslayer <ddsomdim@gmail.com>
2022-09-19 22:14:56 +0300

    Optimized variable names

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -31,28 +31,31 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 def show_all_pokemons(request):
 
-    pokemon_objects = Pokemon.objects.all()
-    pokemon_entities_objects = PokemonEntity.objects.all()
+    time_now = datetime.datetime.now().timestamp()
+    pokemons = Pokemon.objects.all()
+    pokemon_entities = PokemonEntity.objects.filter(
+        appeared_at__it=time_now,
+        disappeared_at__gt=time_now)
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon_entity_object in pokemon_entities_objects:
+    for pokemon_entity in pokemon_entities:
         time_now = datetime.datetime.now().timestamp()
-        pokemon_appeared_time = localtime(pokemon_entity_object.appeared_at).timestamp()
-        pokemon_disappeared_time = localtime(pokemon_entity_object.disappeared_at).timestamp()
+        pokemon_appeared_time = localtime(pokemon_entity.appeared_at).timestamp()
+        pokemon_disappeared_time = localtime(pokemon_entity.disappeared_at).timestamp()
         if pokemon_appeared_time <= time_now <= pokemon_disappeared_time:
             add_pokemon(
-                folium_map, pokemon_entity_object.lat,
-                pokemon_entity_object.lon,
-                request.build_absolute_uri(f'media/{pokemon_entity_object.pokemon.image}')
+                folium_map, pokemon_entity.lat,
+                pokemon_entity.lon,
+                request.build_absolute_uri(f'media/{pokemon_entity.pokemon.image}')
             )
 
 
     pokemons_on_page = []
-    for pokemon_object in pokemon_objects:
+    for pokemon in pokemons:
         pokemons_on_page.append({
-            'pokemon_id':  pokemon_object.id,
-            'img_url': request.build_absolute_uri(f'media/{pokemon_object.image}'),
-            'title_ru': pokemon_object.title,
+            'pokemon_id':  pokemon.id,
+            'img_url': request.build_absolute_uri(f'media/{pokemon.image}'),
+            'title_ru': pokemon.title,
         })
 
     return render(request, 'mainpage.html', context={
@@ -63,48 +66,41 @@ def show_all_pokemons(request):
 
 def show_pokemon(request, pokemon_id):
 
-    time_now = datetime.datetime.now().timestamp()
-    pokemon_objects = Pokemon.objects.filter(id=int(pokemon_id))
-    pokemon_entities_objects = PokemonEntity.objects.filter(
-        pokemon_id=int(pokemon_id),
-        appeared_at__it=time_now,
-        disappeared_at__gt=time_now)
+    pokemons = Pokemon.objects.filter(id=int(pokemon_id))
+    pokemon_entities = PokemonEntity.objects.filter(pokemon_id=int(pokemon_id))
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon_entity_object in pokemon_entities_objects:
+    for pokemon_entity in pokemon_entities:
 
-        # pokemon_appeared_time = localtime(pokemon_entity_object.appeared_at).timestamp()
-        # pokemon_disappeared_time = localtime(pokemon_entity_object.disappeared_at).timestamp()
-        # if pokemon_appeared_time <= time_now <= pokemon_disappeared_time:
         add_pokemon(
-            folium_map, pokemon_entity_object.lat,
-            pokemon_entity_object.lon,
-            request.build_absolute_uri(f'http://127.0.0.1:8000/media/{pokemon_entity_object.pokemon.image}')
+            folium_map, pokemon_entity.lat,
+            pokemon_entity.lon,
+            request.build_absolute_uri(f'http://127.0.0.1:8000/media/{pokemon_entity.pokemon.image}')
         )
 
     pokemons_on_page = {}
-    for pokemon_object in pokemon_objects:
+    for pokemon in pokemons:
         previous_evolution = {}
         next_evolution = {}
-        if pokemon_object.previous_evolution:
+        if pokemon.previous_evolution:
             previous_evolution = {
-                'title_ru': pokemon_object.previous_evolution.title,
-                'pokemon_id': pokemon_object.previous_evolution.id,
-                'img_url': request.build_absolute_uri(f'http://127.0.0.1:8000/media/{pokemon_object.previous_evolution.image}')
+                'title_ru': pokemon.previous_evolution.title,
+                'pokemon_id': pokemon.previous_evolution.id,
+                'img_url': request.build_absolute_uri(f'http://127.0.0.1:8000/media/{pokemon.previous_evolution.image}')
             }
-        if pokemon_object.next_evolution:
+        if pokemon.next_evolution:
             next_evolution = {
-                'title_ru': pokemon_object.next_evolution.title,
-                'pokemon_id': pokemon_object.next_evolution.id,
-                'img_url': request.build_absolute_uri(f'http://127.0.0.1:8000/media/{pokemon_object.next_evolution.image}')
+                'title_ru': pokemon.next_evolution.title,
+                'pokemon_id': pokemon.next_evolution.id,
+                'img_url': request.build_absolute_uri(f'http://127.0.0.1:8000/media/{pokemon.next_evolution.image}')
             }
         pokemons_on_page = {
-            'pokemon_id': pokemon_object.id,
-            'img_url': request.build_absolute_uri(f'http://127.0.0.1:8000/media/{pokemon_entity_object.pokemon.image}'),
-            'title_ru': pokemon_object.title,
-            'description': pokemon_object.description,
-            'title_en': pokemon_object.title_en,
-            'title_jp': pokemon_object.title_jp,
+            'pokemon_id': pokemon.id,
+            'img_url': request.build_absolute_uri(f'http://127.0.0.1:8000/media/{pokemon_entity.pokemon.image}'),
+            'title_ru': pokemon.title,
+            'description': pokemon.description,
+            'title_en': pokemon.title_en,
+            'title_jp': pokemon.title_jp,
             'previous_evolution': previous_evolution,
             'next_evolution': next_evolution
         }
